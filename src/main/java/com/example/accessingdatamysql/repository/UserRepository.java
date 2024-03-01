453523f83a89ab3a6234fdaf58465592c746f36b
Antonis <107072477+vlantonakos@users.noreply.github.com>
2024-03-01 19:30:35 +0200

    remove user

--- src/main/java/com/example/accessingdatamysql/repository/UserRepository.java
+++
@@ -1,9 +0,0 @@
-package com.example.accessingdatamysql.repository;
-
-import org.springframework.data.repository.CrudRepository;
-
-import com.example.accessingdatamysql.entity.User;
-
-public interface UserRepository extends CrudRepository<User, Integer> {
-
-}
